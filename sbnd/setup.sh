e166e6ebf5a8f29f45f3f4b92ac1fca09242c4fd
HaiwangYu <yuhw.pku@gmail.com>
2024-12-20 08:57:43 -0600

    add path-util

--- sbnd/setup.sh
+++ sbnd/setup.sh
@@ -4,6 +4,35 @@ source /exp/sbnd/app/users/yuhw/larsoft/v09_91_02/localProducts_larsoft_v09_91_0
 mrbsetenv
 mrbslp
 
+path-remove ()
+{
+    local IFS=':';
+    local NEWPATH;
+    local DIR;
+    local PATHVARIABLE=${2:-PATH};
+    for DIR in ${!PATHVARIABLE};
+    do
+        if [ "$DIR" != "$1" ]; then
+            NEWPATH=${NEWPATH:+$NEWPATH:}$DIR;
+        fi;
+    done;
+    export $PATHVARIABLE="$NEWPATH"
+}
+
+path-prepend ()
+{
+    path-remove "$1" "$2";
+    local PATHVARIABLE="${2:-PATH}";
+    export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
+}
+
+path-append ()
+{
+    path-remove "$1" "$2";
+    local PATHVARIABLE="${2:-PATH}";
+    export $PATHVARIABLE="${!PATHVARIABLE:+${!PATHVARIABLE}:}$1"
+}
+
 path-prepend /exp/sbnd/app/users/yuhw/opt/lib/ LD_LIBRARY_PATH
 
 path-prepend /exp/sbnd/app/users/yuhw/wct-cfg/cfg WIRECELL_PATH
