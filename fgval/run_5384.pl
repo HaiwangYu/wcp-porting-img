3320b48d9fbd6f2c9855ae90ac78264a28d95c51
Xin Qian <last.qx@gmail.com>
2025-02-19 14:00:04 -0800

    catch up

--- fgval/run_5384.pl
+++ fgval/run_5384.pl
@@ -9,10 +9,12 @@ while(<infile>){
     my $filename = $_;
     chomp($filename);
     $filename =~ /_(\d+)_(\d+)_(\d+)\.root/;
-    print("$filename\n");
+    # print("$filename\n");
     my $runNo = $1;
     my $subRunNo = $2;
     my $eventNo = $3;
+
+    #print "$target_event $eventNo \n";
 
     # Skip if target event specified and doesn't match
     next if (defined $target_event && $eventNo != $target_event);
@@ -24,6 +26,8 @@ while(<infile>){
     #      system("wire-cell -A iname=\"$filename\" -A oname=\"active-clusters-anode_$runNo\_$eventNo\.npz\" -A kind=\"live\" uboone-val.jsonnet &");
     #      system("wire-cell -A iname=\"$filename\" -A oname=\"masked-clusters-anode_$runNo\_$eventNo\.npz\" -A kind=\"dead\" uboone-val.jsonnet &");
     # }
+
+  
 
     if ($i%40 == 39){
        system("wire-cell -l stdout -L debug -A active_clusters=\"active-clusters-anode_$runNo\_$eventNo\.npz\" -A masked_clusters=\"masked-clusters-anode_$runNo\_$eventNo\.npz\" -A bee_zip=$i\.zip -A initial_index=\"$i\" -A initial_runNo=\"$runNo\" -A initial_subRunNo=\"$subRunNo\" -A initial_eventNo=\"$eventNo\" -c ../wct-uboone-clustering.jsonnet > wct_$runNo\_$eventNo\.log");
